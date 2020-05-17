3982af2ecf16fb53aa59ac109aaee832ef6e5e90
Alec Loudenback <alecloudenback@gmail.com>
2020-05-17 15:56:54 -0500

    initial commit with simple example

--- src/Accounting.jl
+++ src/Accounting.jl
@@ -1,5 +1,66 @@
 module Accounting
 
-greet() = print("Hello World!")
+using Transducers
+
+abstract type Ledger end
+
+struct SubLedger 
+    name
+    contents
+end
+
+struct GeneralLedger
+    assets
+    liabilities
+    equity
+end
+
+function GeneralLedger()
+    return GeneralLedger([],[],[])
+end
+
+function addAsset!(gl::GeneralLedger,a)
+    push!(gl.assets,a)
+end
+
+function addLiabilitity!(gl::GeneralLedger,l)
+    push!(gl.liabilities,l)
+end
+
+struct Balance 
+    assets
+    liabilities
+    equity
+end
+
+abstract type ValuationBasis end
+
+
+
+
+abstract type Asset end
+
+abstract type Liability end
+
+
+
+function value(basis,obj)
+    return obj
+end
+
+
+function Balance(basis::ValuationBasis,l::GeneralLedger)
+    val = Map(x -> value(basis,x))
+    a = tcopy(val,l.assets)
+    l = tcopy(val,l.liabilities)
+    a_sum = length(a) == 0 ? 0.0 : reduce(+,a) 
+    l_sum = length(l) == 0 ? 0.0 : reduce(+,l) 
+    eq = a_sum - l_sum
+
+    return Balance(a,l,eq)
+end
+
+export value, Asset, Liability, Balance, GeneralLedger,
+        addAsset!,addLiabilitity!
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,30 @@ using Accounting
 using Test
 
 @testset "Accounting.jl" begin
-    # Write your own tests here.
+
+    # Extending the interface for my own types/valuation basis
+
+    struct MyValuationBasis <: Accounting.ValuationBasis end
+
+    struct MyBond <: Accounting.Asset
+        par
+    end
+
+    Accounting.value(::MyValuationBasis,b::MyBond) = b.par
+
+    gl = GeneralLedger()
+    
+    b = MyBond(100)
+    addAsset!(gl,b)
+
+    bal = Balance(MyValuationBasis(),gl)
+
+    @test sum(bal.assets) == 100.0
+    @test length(bal.liabilities) == 0
+    @test sum(bal.equity) == 100.0
+
+    
+
+
+
 end
